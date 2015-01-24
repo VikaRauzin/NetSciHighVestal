4cfa1ade3b62059fe706d70c249dab5addfc1b9f
Jacob King <jmking628@gmail.com>
2015-01-24 12:48:18 -0500

    Commented

--- nodelistgenerators/side_effects.py
+++ nodelistgenerators/side_effects.py
@@ -1,6 +1,6 @@
 import csv
 import os
-os.chdir("..")
+os.chdir("..")  # moves working directory for data i/o to project master directory
 path_in = "meddra_freq_parsed.tsv"
 path_out = "se_nodelist.csv"
 with open(path_in, 'rb') as fin, open(path_out, 'wb') as fout:
