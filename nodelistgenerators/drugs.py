e74b7d2fa99df449e1ffbef4af6ea6e1bc919580
jmking628 <jmking628@gmail.com>
2015-01-24 11:58:38 -0500

    Create drugs.py

---
+++ nodelistgenerators/drugs.py
@@ -0,0 +1,19 @@
+import csv
+path_in = "meddra_freq_parsed.tsv"
+path_out = "drug_nodelist.csv"
+with open(path_in, 'rb') as fin, open(path_out, 'wb') as fout:
+    fin = csv.reader(fin, delimiter='\t')
+    fout = csv.writer(fout)
+
+    drugs = dict()
+    for line in fin:
+        if line[0] in drugs:  # checks whether drug id is currently in the dictionary
+            drugs[line[0]] += 1  # if it is in the dictionary, increments frequency
+        else:
+            drugs[line[0]] = 1  # if not in the dictionary, adds it with frequency of 1
+
+    header = "Drug ID"  # gephi likes nodelists to have a header row
+    fout.writerow(header)
+    for d in drugs.keys().iteritems():  # iterate dictionary keys, can be modified to include frequency
+        row = d
+        fout.writerow(row)
